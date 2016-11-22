cce2b7b112f2e57a207aacf37d189b115c1abed4
xutl <79109681@qq.com>
2016-11-22 17:06:58 +0800

    c

---
+++ Server.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @link http://www.tintsoft.com/
+ * @copyright Copyright (c) 2012 TintSoft Technology Co. Ltd.
+ * @license http://www.tintsoft.com/license/
+ */
+namespace xutl\swoole;
+
+use yii\base\Component;
+
+class Server extends Component
+{
+    public $host;
+    public $port = 9502;
+    public $mode = SWOOLE_PROCESS;
+    public $sockType;
+    public $config = [];
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+    }
+}
