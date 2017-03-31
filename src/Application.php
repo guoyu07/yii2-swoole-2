544de67af3ef375f9d0482d86653cdc2683ad51b
xuchao <85825770@qq.com>
2017-03-31 19:37:13 +0800

    占坑

---
+++ src/Application.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @link http://www.tintsoft.com/
+ * @copyright Copyright (c) 2012 TintSoft Technology Co. Ltd.
+ * @license http://www.tintsoft.com/license/
+ */
+
+namespace xutl\swoole;
+
+class Application extends \yii\base\Application
+{
+    public function handleRequest($request)
+    {
+        // TODO: Implement handleRequest() method.
+    }
+}
